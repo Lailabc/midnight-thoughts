205ecde3e5b3ff0bf1274c2610b7abb20aee21e4
Lailabc <115160711+Lailabc@users.noreply.github.com>
2022-11-17 15:36:59 +0000

    Add files via upload

---
+++ midnight-thoughts-main/midnight-thoughts-main/js.js
@@ -0,0 +1,10 @@
+
+
+
+// function animateDiv() {
+//     let falling = document.getElementById("falling");
+//     falling.classList.remove("image-container");
+//     falling.classList.add("image-container-down");
+
+// }
+
