7000355bc1984e020059e830ad67c75126b495a3
Lailabc <115160711+Lailabc@users.noreply.github.com>
2022-11-21 17:35:08 +0000

    Add files via upload

---
+++ midnight-thoughts-main/midnight-thoughts-main/midnight-thoughts-main/midnight-thoughts-main/js.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("egg");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+// function animateDiv() {
+//     let falling = document.getElementById("falling");
+//     falling.classList.remove("image-container");
+//     falling.classList.add("image-container-down");
+
+// }
+
